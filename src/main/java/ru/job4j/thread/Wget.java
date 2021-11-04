5e31caacfe73c8254f4804658af397362538c1ac
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-11-04 21:55:54 +0300

    Скачивание файла с ограничением. [#144271 #220966]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -1,9 +1,8 @@
 package ru.job4j.thread;
 
-import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
+import java.util.Scanner;
 
 public class Wget implements Runnable {
     private final String url;
@@ -18,29 +17,40 @@ public class Wget implements Runnable {
     public void run() {
         System.out.println("Скачивание началось....");
         long start = System.currentTimeMillis();
-        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream())) {
-            try (FileOutputStream fileOutputStream = new FileOutputStream("tmp3.jpg")) {
-                byte[] dataBuffer = new byte[speed * 1024];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите адресную строку: ");
+        String s1 = null;
+        try {
+            s1 = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert s1 != null;
+        try (BufferedInputStream in = new BufferedInputStream(new URL(s1).openStream())) {
+            try (FileOutputStream fileOutputStream = new FileOutputStream("tmp4.jpg")) {
+                byte[] dataBuffer = new byte[1024];
                 int bytesRead;
                 long bytesWrited = 0;
-                while ((bytesRead = in.read(dataBuffer, 0, speed * 1024)) != -1) {
+                long time = System.currentTimeMillis();
+                while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
                     fileOutputStream.write(dataBuffer, 0, bytesRead);
                     bytesWrited = bytesWrited + bytesRead;
-                    if (bytesWrited == speed) {
-                        long diffTime = System.currentTimeMillis();
+                    if (bytesWrited > speed) {
+                        long diffTime = System.currentTimeMillis() - time;
                         if (diffTime < 1000) {
                             Thread.sleep(1000 - diffTime);
                         }
                     }
-                    System.out.println(bytesRead);
                     bytesWrited = 0;
+                    System.out.println(bytesRead);
+                    time = System.currentTimeMillis();
                 }
             }
         } catch (IOException | InterruptedException e) {
             Thread.currentThread().interrupt();
         }
         long end = System.currentTimeMillis();
-        System.out.println("Загрузка завершена: " + ((end - start) / 1000));
+        System.out.println("Загрузка завершена: " + ((end - start) / 1000f));
     }
 
     public static void main(String[] args) throws InterruptedException {
@@ -58,5 +68,4 @@ public class Wget implements Runnable {
         }
     }
 }
-
 
