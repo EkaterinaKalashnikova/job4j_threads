97d5e9eaf03fc3fcbf3e0f17ba66a14412a405a8
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-12-09 17:54:50 +0300

    Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,26 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        Base updateBase = memory.computeIfPresent(model.getId(), (key, value) -> {
+            if (value.getVersion() != model.getVersion()) {
+                throw new OptimisticException("Versions are not equal");
+            }
+            return model;
+        });
+        return updateBase != null;
+    }
+
+    public void delete(Base model) {
+        memory.remove(model.getId());
+    }
+}
---
+++ src/test/java/ru/job4j/cache/CacheTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.cache;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CacheTest {
+
+    @Test
+    public void whenAddValue() {
+        Cache cache = new Cache();
+        Base model = new Base(1, 1);
+        boolean b = cache.add(model);
+        Assert.assertTrue(b);
+    }
+
+    @Test
+    public void whenUpdateValueNotChange() {
+        Cache cache = new Cache();
+        Base model = new Base(1, 1);
+        cache.add(model);
+        Assert.assertTrue(cache.update(model));
+    }
+
+    @Test
+    public void whenUpdateValueChange() {
+        Cache cache = new Cache();
+        Base model = new Base(1, 1);
+        Base modelNext = new Base(2, 1);
+        cache.add(model);
+        Assert.assertFalse(cache.update(modelNext));
+    }
+
+    @Test(expected = OptimisticException.class)
+    public void whenUpdateValueWhereChangeVersion() {
+        Cache cache = new Cache();
+        Base model = new Base(1, 1);
+        Base modelNext = new Base(1, 2);
+        cache.add(model);
+        cache.add(modelNext);
+        cache.update(modelNext);
+    }
+
+    @Test
+    public void whenDeleteValue() {
+        Cache cache = new Cache();
+        Base model = new Base(1, 1);
+        cache.add(model);
+        cache.delete(model);
+        Assert.assertTrue(cache.add(model));
+    }
+}
