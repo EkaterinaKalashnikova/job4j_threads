04f322ee9d699461aa9f7edd5e1135941c1573a7
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-12-16 22:19:28 +0300

    ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,28 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public void emailTo(User user) {
+        pool.submit(() -> {
+            String subject = String.format("subject = Notification {username} to email {email}",
+                    user.getUsername(), user.getEmail());
+            String body = String.format("Add a new event to {username}", user.getUsername());
+            String email = String.format("to the new address", user.getEmail());
+            send(subject, body, email);
+        });
+    }
+
+    public void close() {
+        pool.shutdown();
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+}
