36f560fb21220c09021ad5707470a4f5fc8216bf
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-12-31 10:20:01 +0300

    ForkJoinPool [#315067]

--- src/main/java/ru/job4j/pool/SearchIndex.java
+++ src/main/java/ru/job4j/pool/SearchIndex.java
@@ -3,10 +3,21 @@ package ru.job4j.pool;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class SearchIndex {
+public class SearchIndex<T> extends RecursiveTask<Integer> {
+    private final T[] data;
+    private final int start;
+    private final int end;
+    private final T searchElement;
 
-    public static <T> int linearSearch(T[] data, T searchElement, int start, int end) {
-        for (int i = start; i < end; i++) {
+    public SearchIndex(T[] data, int start, int end, T searchElement) {
+        this.data = data;
+        this.start = start;
+        this.end = end;
+        this.searchElement = searchElement;
+    }
+
+    public <T> int linearSearch(T[] data, T searchElement, int start, int end) {
+        for (int i = start; i <= end; i++) {
             if (data[i].equals(searchElement)) {
                 return i;
             }
@@ -14,43 +25,26 @@ public class SearchIndex {
         return -1;
     }
 
-    public static <T> int recursiveBinarySearch(T[] data, int start, int end, T searchElement) {
+    public <T> int recursiveBinarySearch(T[] data, int start, int end, T searchElement) {
         int mid = (end - start) / 2;
+        SearchIndexArray<T> leftSearch = new SearchIndexArray<>(data, start, mid, searchElement);
+        SearchIndexArray<T> rightSearch = new SearchIndexArray<>(data, mid + 1, end, searchElement);
+        leftSearch.fork();
+        rightSearch.fork();
+        return Math.max(leftSearch.join(), rightSearch.join());
+    }
+
+    @Override
+    protected Integer compute() {
         if (end - start <= 10) {
-            return mid;
+            return linearSearch(data, searchElement, start, end);
         }
-        SearchIndexArray<T> leftSort = new SearchIndexArray<>(data, start, mid, searchElement);
-        SearchIndexArray<T> rightSort = new SearchIndexArray<>(data, mid + 1, end, searchElement);
-        leftSort.fork();
-        rightSort.fork();
-        return Math.max(leftSort.join(), rightSort.join());
+        return recursiveBinarySearch(data, start, end, searchElement);
     }
 
-    public static <T> int init(T[] data, int start, int end, T searchElement) {
+    public static <T> int init(T[] data, T searchElement) {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        return forkJoinPool.invoke(new SearchIndexArray<>(data, start, end, searchElement));
-    }
-
-    public static class Sarch<T> extends RecursiveTask<Integer> {
-        private final T[] data;
-        private final int start;
-        private final int end;
-        private final T searchElement;
-
-        public Sarch(T[] data, int start, int end, T searchElement) {
-            this.data = data;
-            this.start = start;
-            this.end = end;
-            this.searchElement = searchElement;
-        }
-
-        @Override
-        protected Integer compute() {
-            if (end - start < 10) {
-                return linearSearch(data, searchElement, start, end);
-            }
-            return recursiveBinarySearch(data, start, end, searchElement);
-        }
+        return forkJoinPool.invoke(new SearchIndex<>(data, 0, data.length, searchElement));
     }
 }
 
--- src/test/java/ru/job4j/pool/SearchIndexTest.java
+++ src/test/java/ru/job4j/pool/SearchIndexTest.java
@@ -8,35 +8,35 @@ public class SearchIndexTest {
     @Test
     public void indexToArray() {
         Integer[] data = new Integer[]{45, 89, 63, 47, 15, 2, 0, 98, 36, 115, 6, 96};
-        Integer ints = SearchIndex.init(data, 0, data.length, 115);
+        Integer ints = SearchIndex.init(data, 115);
         Assert.assertThat(9, Is.is(ints));
     }
 
     @Test
     public void indexToArrayOne() {
         Integer[] data = new Integer[]{45};
-        Integer ints = SearchIndex.init(data, 0, data.length, 45);
+        Integer ints = SearchIndex.init(data, 45);
         Assert.assertThat(0, Is.is(ints));
     }
 
     @Test
     public void indexNotToArray() {
         Integer[] data = new Integer[]{45, 89, 63, 47, 15, 2, 0, 98, 36, 115, 6, 96};
-        Integer ints = SearchIndex.init(data, 0, data.length, 7);
+        Integer ints = SearchIndex.init(data, 7);
         Assert.assertThat(-1, Is.is(ints));
     }
 
     @Test
     public void indexStartToArray() {
         Integer[] data = new Integer[]{45, 89, 63, 47, 15, 2, 0, 98, 36, 115, 6, 96, 14, 78, 12, 10, 8, 48, 35, 5, 33, 41};
-        Integer ints = SearchIndex.init(data, 0, data.length, 89);
+        Integer ints = SearchIndex.init(data, 89);
         Assert.assertThat(1, Is.is(ints));
     }
 
     @Test
     public void indexEndToArray() {
         Integer[] data = new Integer[]{45, 89, 63, 47, 15, 2, 0, 98, 36, 115, 6, 96, 14, 78, 12, 10, 8, 48, 35, 5, 33, 41};
-        Integer ints = SearchIndex.init(data, 0, data.length, 41);
+        Integer ints = SearchIndex.init(data, 41);
         Assert.assertThat(21, Is.is(ints));
     }
 
@@ -46,7 +46,7 @@ public class SearchIndexTest {
         for (int i = 0; i < 100000000; i++) {
             data[i] = (int) (Math.random() * 100000000);
         }
-        SearchIndex.init(data, 0, data.length, 101);
+        SearchIndex.init(data, 101);
         System.out.println(101);
     }
 
@@ -65,7 +65,7 @@ public class SearchIndexTest {
             }
         }
         long finish = System.currentTimeMillis();
-        SearchIndex.init(data, 0, data.length, 101);
+        SearchIndex.init(data, 101);
         long end = System.currentTimeMillis();
         System.out.println(finish - start);
         System.out.println(end - finish);
